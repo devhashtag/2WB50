8919e0cc6641f6fdb2c9c8a6714464572db497bf
Peter <p.j.heijstek@student.tue.nl>
2021-02-10 14:44:41 +0100

    add wrapper class

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,9 +1,29 @@
 # %%
 from scipy import stats
+from scipy.special import comb
 print('Hello, world!')
 
 # %%
 # Initiate given values
+class Probabilities:
+    def __init__(self, n, T):
+        self.n = n
+        self.T = T
+
+    def revenue(self, a):
+        return a**0.5
+    
+    def click(self, a):
+        return comb(self.n, a) * (0.2**a) * (0.8)**(self.n-a)
+    
+    def scan_next(self, a):
+        return 99/100
+
+    def scan_next_after_click(self, a):
+        return 7/10
+
+probs = Probabilities(10, 5)
+
 A = [i for i in range(1, 11)]
 T = []
 pA = []
