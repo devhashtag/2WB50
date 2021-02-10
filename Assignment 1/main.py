6ae1b4d36a7d2a760e7635643052cdac360e4c13
Milan van Gool <m.v.gool@student.tue.nl>
2021-02-10 14:20:24 +0100

    Basic layout

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,4 +1,20 @@
 # %%
+from scipy import stats
 print('Hello, world!')
 
 # %%
+# Initiate given values
+A = [i for i in range(1, 11)]
+T = []
+pA = []
+vA = [a**(1/2) for a in A]
+alphaA = 7/10
+betaA = 99/100
+
+# %%
+# Policy 1
+pi = A
+pi
+
+# %%
+# Policy 2
